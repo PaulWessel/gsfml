96b8161c8f6049df69f99f1816fa1ba10c24d1aa
Paul Wessel <pwessel@hawaii.edu>
2020-04-25 18:29:34 -1000

    Eliminate old module glue

---
+++ TOOLS/src/gmt_gsfml_moduleinfo.h
@@ -0,0 +1,4 @@
+	{"fzanalyzer", "fzanalyzer", "gsfml", "Analysis of fracture zones using crossing profiles", "<DI,>DO"},
+	{"fzblender", "fzblender", "gsfml", "Produce a smooth blended FZ trace", "<DI,>DO"},
+	{"mlconverter", "mlconverter", "gsfml", "Convert chrons to ages using selected magnetic timescale", "<DI,>DO"},
+	
