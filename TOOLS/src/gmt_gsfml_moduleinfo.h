e8ffd1210e98fb2f90b80809a1ecfa26887d47e2
Paul Wessel <pwessel@hawaii.edu>
2020-04-25 19:10:36 -1000

    Finalize

--- TOOLS/src/gmt_gsfml_moduleinfo.h
+++
@@ -1,4 +0,0 @@
-	{"fzanalyzer", "fzanalyzer", "gsfml", "Analysis of fracture zones using crossing profiles", "<DI,>DO"},
-	{"fzblender", "fzblender", "gsfml", "Produce a smooth blended FZ trace", "<DI,>DO"},
-	{"mlconverter", "mlconverter", "gsfml", "Convert chrons to ages using selected magnetic timescale", "<DI,>DO"},
-	
